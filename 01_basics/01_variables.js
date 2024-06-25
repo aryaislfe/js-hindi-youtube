b2093df45a468d94148c8b4d7b59657a17250ffa
Saurabh Yadav <srkyad7618@gmail.com>
2024-06-25 05:55:42 +0000

    m

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,22 @@
-const
+const accountid =123456
+let accountEmail ="arya568@gmail.com"
+var accountPassword ="1234536"
+accountCity ="New Delhi"
+let accountState;
+
+// accountid = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword="21212121"
+accountCity="Bengluru"
+
+console.log(accountid);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
